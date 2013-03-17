846892b2ccf6d09000c1937087bea21ba4ad7d31
Claus <claus1478@gmail.com>
2013-03-17 17:50:21 -0400

    added localization classes

--- Start Robot/src/localization/Localization.java
+++ Start Robot/src/localization/Localization.java
@@ -4,9 +4,10 @@ package localization;
  * This class will serve as a map for the robot. It will localize itself, the ball dispenser, the
  * zones, and the goal.
  * @author Tuan-Kiet Luu
+ * @author Kornpat Choy (Claus)
  * @version 1.0
  */
 
 public class Localization {
-
+	
 }
