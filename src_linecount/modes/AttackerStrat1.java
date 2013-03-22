cfc3379ad22b49901028726ebda3e166783f111a
Claus <claus1478@gmail.com>
2013-03-22 10:51:04 -0400

    Revert "line count"
    This reverts commit 40a6aeef780a3819c87737dfa6be3851cf1c8841.

--- src_linecount/modes/AttackerStrat1.java
+++
@@ -1,9 +0,0 @@
-package modes;
-/**
- * The Class AttackerStrat1.
- * 
- * @author Tuan-Kiet
- */
-public class AttackerStrat1 {
-
-}
--- src_linecount/modes/DefenderStrat2.java
+++
@@ -1,9 +0,0 @@
-package modes;
-/**
- * This class will be the code for the Defender Strategy 2.
- * @author Tuan-Kiet Luu
- * @version 1.0
- */
-public class DefenderStrat2 {
-
-}
