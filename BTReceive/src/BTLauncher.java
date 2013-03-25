094780ea334bc80d8465d1cfde806bdf5714184b
LuuTK <tuan-kiet.luu@mail.mcgill.ca>
2013-03-25 11:48:54 -0400

    Added BTLauncher, which is the final class that would be used for the launcher, Not tested yet.

--- BTReceive/src/BTLauncher.java
+++ BTReceive/src/BTLauncher.java
@@ -1,4 +1,3 @@
-
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 
@@ -20,18 +19,18 @@ import lejos.nxt.comm.Bluetooth;
  * This class should be opened in the launcher brick. In summary, it will always be
  * waiting for a signal to shoot. When the master brick  connects to this brick, it will shoot.
  * 
- * 
  * @author Lawrie Griffiths
  * @author Tuan-Kiet Luu
  * @author Nabil Zoldjalali
  *
  */
-public class BTReceive {
-	
-	static int speed = 3000; //speed of launch
-	static int returnSpeed = 80;	 //speed of return to original position
-	static int angle = -80; //angle the arm turns to (final position of catapult arm) - negative because initally not geared 
-	static int reAdjust = 10;	
+public class BTLauncher {
+
+
+static int speed = 3000; //speed of launch
+static int returnSpeed = 80;	 //speed of return to original position
+static int angle = -80; //angle the arm turns to (final position of catapult arm) - negative because initally not geared 
+static int reAdjust = 10;	
 
 	public static void main(String [] args)  throws Exception 
 	{
@@ -39,52 +38,68 @@ public class BTReceive {
         String waiting = "Waiting...!!!";
         String closing = "Closing...";
     
+    	
+    	
 		while (true)
 		{
+			
+			int turn = 180; 
+			int angle1 = 90;
 			NXTRegulatedMotor leftMotor = Motor.A;
+			NXTRegulatedMotor turnMotor = Motor.C;
 			LCD.drawString(waiting,0,0);
 			LCD.refresh();
-			
+
 
 	        BTConnection btc = Bluetooth.waitForConnection();
-	        
+
 			LCD.clear();
 			LCD.drawString(connected,0,0);
+			/* START MOVE ROBOT HERE; INSERT ALGORITHM TODO*/
 			
-			/* START MOVE ROBOT HERE */ 
+			//LCD.drawString("got something", 4, 4);
+			//leftMotor.rotate(100);
+			//LCD.refresh();	
 			
-	//		LCD.drawString("got something", 4, 4);
-			leftMotor.rotate(720);
-			LCD.refresh();	
-			
-			
-			
-			
-			
-			
-			
+			/*for( x = 0; x<1000; x++){
 
-			/* END MOVE ROBOT HERE */
+			x=x+6;
+			x=x-58;
+			x=x+53;
+
+
+			}*/
+			
+			for(int i= 0; i<5; i++){
+			Motor.C.setSpeed(turn);
+			//Motor.C.rotateTo(angle);
+			Motor.C.rotate(angle1);
+
+
+			try {
+			Thread.sleep(4000);
+			} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			}
+
+
+			Motor.A.setSpeed(speed);
+			Motor.B.setSpeed(speed);
+			Motor.A.rotateTo(angle, true);
+			Motor.B.rotateTo(angle);
+
+			//do return to original position
+			Motor.A.setSpeed(returnSpeed);
+			Motor.B.setSpeed(returnSpeed);
+			Motor.A.rotateTo(reAdjust, true);
+			Motor.B.rotateTo(reAdjust);
+
+			/* END MOVE ROBOT HERE; INSERT ALGORITHM TODO */
+			
 			DataInputStream dis = btc.openDataInputStream();
 			DataOutputStream dos = btc.openDataOutputStream();
-			
-			
-		/*	
-			if(dis.readInt() == 1){
-				LCD.drawString("got 1", 4, 4);
-				Sound.beep();
-			}
-			
-			for(int i=0;i<100;i++) {
-				int n = dis.readInt();
-				LCD.drawInt(n,7,0,1);
-				LCD.refresh();
-				dos.writeInt(-n);
-				dos.flush();
-			}
-			
-			
-			*/
+
 			dis.close();
 			dos.close();
 			Thread.sleep(100); // wait for data to drain
@@ -95,5 +110,5 @@ public class BTReceive {
 			LCD.clear();
 		}
 	}
-}
-
+	}
+	}
