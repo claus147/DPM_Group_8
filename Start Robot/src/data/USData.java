26b360ad775e962b59c97b779b931c9e69cbdcac
Claus <claus1478@gmail.com>
2013-03-26 20:12:40 -0400

    starting the usdata again

--- Start Robot/src/data/USData.java
+++ Start Robot/src/data/USData.java
@@ -1,9 +1,5 @@
 package data;
 
-//<<<<<<< HEAD
-//=======
-import lejos.nxt.LCD;
-//>>>>>>> update
 import lejos.nxt.SensorPort; 
 import lejos.nxt.Sound;
 import lejos.nxt.UltrasonicSensor;
@@ -19,255 +15,259 @@ import lejos.util.TimerListener;
  * @version 1.1
  */
 
-
-//<<<<<<< HEAD
-public class USData {//implements TimerListener{
-
+public class USData implements TimerListener{
 	
+	private int noWall = 80;
+	private Timer timer;
+	private int sleepTime = 50; //optimal sleepTime
+	private boolean isWall = false;
+	private int usData;
+	private UltrasonicSensor us = new UltrasonicSensor(SensorPort.S1); //default port
 	
-	public static SensorPort usPortA = SensorPort.S2;
-	public static SensorPort usPortB = SensorPort.S3;
-	public static UltrasonicSensor us1 = new UltrasonicSensor(usPortA);
-	public static UltrasonicSensor us2 = new UltrasonicSensor(usPortB);
-	
-	
-	public static int getUS1Data (){
-		return us1.getDistance();
+	/**
+	 * constructor
+	 * default ultrasonic sensor port w/ default 50 sleep time (optimal)
+	 */
+	public USData(){	
+		this.timer = new Timer(sleepTime,this);
 	}
 	
-	public static int getFilteredUS1Data(){
-		int distance;
-		int filtered = 0;
-		int filterControl = 0;
-		int FILTER_OUT = 20;
-	    // there will be a delay here
-		distance = us1.getDistance();
-		
-		// rudimentary filter
-		if (distance == 255 && filterControl < FILTER_OUT) {
-			// bad value, do not set the distance var, however do increment the filter value
-			filterControl ++;
-		} else if (distance == 255){
-			// true 255, therefore set distance to 255
-			filtered = distance;
-		} else {
-			// distance went below 255, therefore reset everything.
-			filterControl = 0;
-			filtered = distance;
+	
+	/**
+	 * constructor
+	 * pass in US sensor w/ default 50 sleep time (optimal)
+	 * @param us - the light sensor to use
+	 */
+	public USData(UltrasonicSensor us){	
+		this.us = us;
+		this.timer = new Timer(sleepTime,this);
+	}
+	
+	/**
+	 * constructor
+	 * pass in US sensor & user defined sleep time
+	 * @param us - the light sensor to use
+	 * @param sleepTime - can choose the sleep time
+	 */
+	public USData(UltrasonicSensor us, int sleepTime){	
+		this.us =us;
+		this.sleepTime = sleepTime;
+		this.timer = new Timer(this.sleepTime,this);
+	}
+	
+	public void timedOut() {
+		setIsWall(false);
+		if(getUSData(usData)<=noWall){
+			setIsWall(true);
+			Sound.twoBeeps();
 		}
-		
-		return filtered;
-	}
-	
-	public static int getFilteredUS2Data(){
-//=======
-public class USData extends Thread{//implements TimerListener{
-
-	
-	public static int us1Data = 0;
-	public static int us2Data = 0;
-	public static UltrasonicSensor us1 = new UltrasonicSensor(SensorPort.S2);
-	public static UltrasonicSensor us2  = new UltrasonicSensor(SensorPort.S3);
-	public USData(){
-		
-	}
-	
-	public void run(){
-		while(true){
-			us1Data = us1.getDistance();
-			us2Data = us2.getDistance();
-			try {
- 			Thread.sleep(50);
-			} catch (InterruptedException e) {
- 			
-			}
-			LCD.drawString(""+us1Data, 0, 8);
-		}
-		
 	}
 	
 	
-	public static int getFilteredUS1Data(){
-//>>>>>>> update
-		int distance;
-		int filtered = 0;
-		int filterControl = 0;
-		int FILTER_OUT = 20;
-	    // there will be a delay here
-//<<<<<<< HEAD
-		distance = us2.getDistance();
+	/**
+	 * This method will take the distance using the ultrasonic Sensor
+	 * @return usData
+	 */
+	public double getUSData(int usData){
+		// do a ping
+		// us.ping();
+		usData = us.getDistance();
 		
-		// rudimentary filter
-		if (distance == 255 && filterControl < FILTER_OUT) {
-			// bad value, do not set the distance var, however do increment the filter value
-			filterControl ++;
-		} else if (distance == 255){
-			// true 255, therefore set distance to 255
-			filtered = distance;
-		} else {
-			// distance went below 255, therefore reset everything.
-			filterControl = 0;
-			filtered = distance;
-		}
-		
-		return filtered;
+		//TODO : CHANGE THIS CONDITION
+		 if (usData > noWall){				//if distance > 80 (noWall) set it to 80
+			 usData = noWall;
+		 }
+		return usData;
+	}
+	
+	/**
+	 * starts the timer (default already started)
+	 */
+	public void start(){
+		timer.start();
+		setIsWall(false);
+	}
+	
+	/**
+	 * stop the timer
+	 */
+	public void stop(){
+		timer.stop();
 	}
 
-	
-	public static int getUS2Data (){
-		return us2.getDistance();
+	/**
+	 * set true or false (if there is a wall)
+	 * @param isLine
+	 */
+	public void setIsWall(boolean isWall){
+		this.isWall = isWall;
 	}
 	
-//=======
-		distance = us1.getDistance();
-		
-		// rudimentary filter
-		if (distance == 255 && filterControl < FILTER_OUT) {
-			// bad value, do not set the distance var, however do increment the filter value
-			filterControl ++;
-		} else if (distance == 255){
-			// true 255, therefore set distance to 255
-			filtered = distance;
-		} else {
-			// distance went below 255, therefore reset everything.
-			filterControl = 0;
-			filtered = distance;
-		}
-		
-		return filtered;
+	/**
+	 * 
+	 * @return true if wall detected, false otherwise
+	 */
+	public boolean getIsWall(){
+		return isWall;
 	}
-	
-	public static int getFilteredUS2Data(){
-		int distance;
-		int filtered = 0;
-		int filterControl = 0;
-		int FILTER_OUT = 20;
-	    // there will be a delay here
-		distance = us2.getDistance();
-		
-		// rudimentary filter
-		if (distance == 255 && filterControl < FILTER_OUT) {
-			// bad value, do not set the distance var, however do increment the filter value
-			filterControl ++;
-		} else if (distance == 255){
-			// true 255, therefore set distance to 255
-			filtered = distance;
-		} else {
-			// distance went below 255, therefore reset everything.
-			filterControl = 0;
-			filtered = distance;
-		}
-		
-		return filtered;
-	}
-
-	public int getUS1Data (){
-		return us1.getDistance();
-	}
-	public int getUS2Data (){
-		return us2.getDistance();
-	}
-	
-//>>>>>>> update
-	
-	
-	
-//	
-//	private int noWall = 80;
-//	private Timer timer;
-//	private int sleepTime = 50; //optimal sleepTime
-//	private boolean isWall = false;
-//	private int usData;
-//	
-//	/**
-//	 * constructor
-//	 * default ultrasonic sensor port w/ default 50 sleep time (optimal)
-//	 */
-//	public USData(){	
-//		this.timer = new Timer(sleepTime,this);
-//	}
-//	
-//	
-//	/**
-//	 * constructor
-//	 * pass in US sensor w/ default 50 sleep time (optimal)
-//	 * @param us - the light sensor to use
-//	 */
-//	public USData(UltrasonicSensor us){	
-//		this.us = us;
-//		this.timer = new Timer(sleepTime,this);
-//	}
-//	
-//	/**
-//	 * constructor
-//	 * pass in US sensor & user defined sleep time
-//	 * @param us - the light sensor to use
-//	 * @param sleepTime - can choose the sleep time
-//	 */
-//	public USData(UltrasonicSensor us, int sleepTime){	
-//		this.us =us;
-//		this.sleepTime = sleepTime;
-//		this.timer = new Timer(this.sleepTime,this);
-//	}
-//	
-//	public void timedOut() {
-//		setIsWall(false);
-//		if(getUSData(usData)<=noWall){
-//			setIsWall(true);
-//			Sound.twoBeeps();
-//		}
-//	}
-//	
-//	
-//	/**
-//	 * This method will take the distance using the ultrasonic Sensor
-//	 * @return usData
-//	 */
-//	public double getUSData(int usData){
-//		// do a ping
-//		// us.ping();
-//		usData = us.getDistance();
-//		
-//		//TODO : CHANGE THIS CONDITION
-//		 if (usData > noWall){				//if distance > 80 (noWall) set it to 80
-//			 usData = noWall;
-//		 }
-//		return usData;
-//	}
-//	
-//	/**
-//	 * starts the timer (default already started)
-//	 */
-//	public void start(){
-//		timer.start();
-//		setIsWall(false);
-//	}
-//	
-//	/**
-//	 * stop the timer
-//	 */
-//	public void stop(){
-//		timer.stop();
-//	}
-//
-//	/**
-//	 * set true or false (if there is a wall)
-//	 * @param isLine
-//	 */
-//	public void setIsWall(boolean isWall){
-//		this.isWall = isWall;
-//	}
-//	
-//	/**
-//	 * 
-//	 * @return true if wall detected, false otherwise
-//	 */
-//	public boolean getIsWall(){
-//		return isWall;
-//	}
 
 
 
 	
 
 }
+
+
+////<<<<<<< HEAD
+//public class USData {//implements TimerListener{
+//
+//	
+//	
+//	public static SensorPort usPortA = SensorPort.S2;
+//	public static SensorPort usPortB = SensorPort.S3;
+//	public static UltrasonicSensor us1 = new UltrasonicSensor(usPortA);
+//	public static UltrasonicSensor us2 = new UltrasonicSensor(usPortB);
+//	
+//	
+//	public static int getUS1Data (){
+//		return us1.getDistance();
+//	}
+//	
+//	public static int getFilteredUS1Data(){
+//		int distance;
+//		int filtered = 0;
+//		int filterControl = 0;
+//		int FILTER_OUT = 20;
+//	    // there will be a delay here
+//		distance = us1.getDistance();
+//		
+//		// rudimentary filter
+//		if (distance == 255 && filterControl < FILTER_OUT) {
+//			// bad value, do not set the distance var, however do increment the filter value
+//			filterControl ++;
+//		} else if (distance == 255){
+//			// true 255, therefore set distance to 255
+//			filtered = distance;
+//		} else {
+//			// distance went below 255, therefore reset everything.
+//			filterControl = 0;
+//			filtered = distance;
+//		}
+//		
+//		return filtered;
+//	}
+//	
+//	public static int getFilteredUS2Data(){
+////=======
+//public class USData extends Thread{//implements TimerListener{
+//
+//	
+//	public static int us1Data = 0;
+//	public static int us2Data = 0;
+//	public static UltrasonicSensor us1 = new UltrasonicSensor(SensorPort.S2);
+//	public static UltrasonicSensor us2  = new UltrasonicSensor(SensorPort.S3);
+//	public USData(){
+//		
+//	}
+//	
+//	public void run(){
+//		while(true){
+//			us1Data = us1.getDistance();
+//			us2Data = us2.getDistance();
+//			try {
+// 			Thread.sleep(50);
+//			} catch (InterruptedException e) {
+// 			
+//			}
+//			LCD.drawString(""+us1Data, 0, 8);
+//		}
+//		
+//	}
+//	
+//	
+//	public static int getFilteredUS1Data(){
+////>>>>>>> update
+//		int distance;
+//		int filtered = 0;
+//		int filterControl = 0;
+//		int FILTER_OUT = 20;
+//	    // there will be a delay here
+////<<<<<<< HEAD
+//		distance = us2.getDistance();
+//		
+//		// rudimentary filter
+//		if (distance == 255 && filterControl < FILTER_OUT) {
+//			// bad value, do not set the distance var, however do increment the filter value
+//			filterControl ++;
+//		} else if (distance == 255){
+//			// true 255, therefore set distance to 255
+//			filtered = distance;
+//		} else {
+//			// distance went below 255, therefore reset everything.
+//			filterControl = 0;
+//			filtered = distance;
+//		}
+//		
+//		return filtered;
+//	}
+//
+//	
+//	public static int getUS2Data (){
+//		return us2.getDistance();
+//	}
+//	
+////=======
+//		distance = us1.getDistance();
+//		
+//		// rudimentary filter
+//		if (distance == 255 && filterControl < FILTER_OUT) {
+//			// bad value, do not set the distance var, however do increment the filter value
+//			filterControl ++;
+//		} else if (distance == 255){
+//			// true 255, therefore set distance to 255
+//			filtered = distance;
+//		} else {
+//			// distance went below 255, therefore reset everything.
+//			filterControl = 0;
+//			filtered = distance;
+//		}
+//		
+//		return filtered;
+//	}
+//	
+//	public static int getFilteredUS2Data(){
+//		int distance;
+//		int filtered = 0;
+//		int filterControl = 0;
+//		int FILTER_OUT = 20;
+//	    // there will be a delay here
+//		distance = us2.getDistance();
+//		
+//		// rudimentary filter
+//		if (distance == 255 && filterControl < FILTER_OUT) {
+//			// bad value, do not set the distance var, however do increment the filter value
+//			filterControl ++;
+//		} else if (distance == 255){
+//			// true 255, therefore set distance to 255
+//			filtered = distance;
+//		} else {
+//			// distance went below 255, therefore reset everything.
+//			filterControl = 0;
+//			filtered = distance;
+//		}
+//		
+//		return filtered;
+//	}
+//
+//	public int getUS1Data (){
+//		return us1.getDistance();
+//	}
+//	public int getUS2Data (){
+//		return us2.getDistance();
+//	}
+//	
+////>>>>>>> update
+	
+	
+	
+	
--- Start Robot/src/main/USLocTest.java
+++ Start Robot/src/main/USLocTest.java
@@ -2,6 +2,7 @@ package main;
 
 import lejos.nxt.SensorPort;
 import lejos.nxt.UltrasonicSensor;
+import modes.Controller;
 import motion.Navigation;
 import localization.USLocalizer;
 import odometry.Odometry;
@@ -22,7 +23,8 @@ public class USLocTest{
 		
 		Navigation nav = new Navigation(odo);
 		
-		LCDinfo info = new LCDinfo(odo);
+		Controller con = new Controller(odo);
+		LCDinfo info = new LCDinfo(odo, con);
 		
 		USLocalizer usLoc = new USLocalizer(odo, usL, usR, USLocalizer.LocalizationType.RISING_EDGE);
 		usLoc.doLocalization();
