6dd13498b2e80eb2fa9ebfe46b1e2ec4cf9dbbaf
Claus <claus1478@gmail.com>
2013-04-02 14:42:50 -0400

    YY again

--- Start Robot/src/modes/AttackerMode.java
+++ Start Robot/src/modes/AttackerMode.java
@@ -13,6 +13,7 @@ import lejos.nxt.UltrasonicSensor;
 import localization.LightLocalizer;
 import localization.USLocalizer;
 import motion.Navigation;
+import motion.SquareNavigation;
 import connection.StartCorner;
 
 
@@ -25,19 +26,21 @@ import connection.StartCorner;
  */
 public class AttackerMode {
 
-	public static Odometry odo;
+	public static Odometry odo = new Odometry(true);;
 	Controller control = new Controller(odo);
-	private LightSensor lsr = new LightSensor(SensorPort.S4);
-	private LightSensor lsl = new LightSensor(SensorPort.S1);
-	UltrasonicSensor usL = new UltrasonicSensor(SensorPort.S2);
-	UltrasonicSensor usR = new UltrasonicSensor(SensorPort.S3);
 	
-	public static USData usData = new USData();
+	private LightSensor lsl = new LightSensor(SensorPort.S1);
+	private LightSensor lsr = new LightSensor(SensorPort.S4);
+	public UltrasonicSensor usl = new UltrasonicSensor(SensorPort.S2);
+	public UltrasonicSensor usr = new UltrasonicSensor(SensorPort.S3);
+	
+	public USData usDataL = new USData(usl);
+	public USData usDataR = new USData(usr);
 	Navigation navigate = new Navigation(odo);
 	StartCorner sc = StartCorner.BOTTOM_LEFT;
+	SquareNavigation sqrNav = new SquareNavigation(odo, lsl, lsr, usDataL, usDataR);
 
-
-	
+	LCDinfo lcd = new LCDinfo(odo);
 	/**
 	 * TODO : develop an attacker algorithm
 	 * Testing class;
@@ -45,8 +48,8 @@ public class AttackerMode {
 	 */
 	public void attackAlgorithm(){
 		
-		odo = new Odometry(true);
-		LCDinfo lcd = new LCDinfo(odo, control);
+		
+		
 		lcd.start();
 		
 		
@@ -58,6 +61,13 @@ public class AttackerMode {
 	 * TODO: Call the strategy established in AttackerStrat1
 	 */
 	public void setStrategy(){
+		
+		
+		//31 March test!
+		
+		sqrNav.travelTo(0, 210);
+		
+		
 		
 		/**
 		 * 1. The robot is placed in Corner 0. You will be asked to orient the robot in a 
@@ -80,56 +90,56 @@ public class AttackerMode {
 			8. The robot then returns to the starting position and halts.
 		 */
 
-		double launchingAngle = 0;
-		//launch X and Y are the coordinates of the goal - 8 feet
-		double launchXBlock = 0;
-		double launchYBlock = 0;
-		double launchX = 0;
-		double launchY = 0;
-		
-		//BluetoothConnection bc = new BluetoothConnection(); //must pass this in here somewhere /************used
-		
-		//Transmission trans = bc.getTransmission();//new Transmission(); //**********used*******
-		double goalXBlock = 5;//trans.w1;
-		double goalYBlock = 9;//trans.w2;
-		if(goalYBlock > 6){ 
-			launchXBlock = goalXBlock;
-			launchYBlock = goalYBlock - 8;
-			launchingAngle = 0;
-		}else{
-			launchXBlock = goalXBlock;
-			launchYBlock = goalYBlock - 8;
-			launchingAngle = 180;
-		}
-		launchX = launchXBlock * 30;
-		launchY = launchYBlock * 30;
-		/* step 1 */
-		
-		
-		/* step 2 DONE */
-		/* step 3 */
-		// ???
-		/* step 4 */// LOCALIZATIONS
-		//usData.start();
-		USLocalizer usLoc = new USLocalizer(odo, navigate ,usL, usR, USLocalizer.LocalizationType.RISING_EDGE);
-		usLoc.doLocalization();
-		LightLocalizer lightLocalizer = new LightLocalizer(odo, navigate, lsl, lsr, sc);
-		lightLocalizer.doLocalization();
-//		try { Thread.sleep(2000); } catch (InterruptedException e) {}
-		/* step 5 + step 6, Obstacle avoidance not working yet */
-			control.travelTo(60,60);
-			control.turnTo(launchingAngle );
-		/* step 7 */
-		BTConnectTest connect = new BTConnectTest();
-		try {
-			connect.connect();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		/* step 8 */
-		control.travelTo(0, 0);
-	
+//		double launchingAngle = 0;
+//		//launch X and Y are the coordinates of the goal - 8 feet
+//		double launchXBlock = 0;
+//		double launchYBlock = 0;
+//		double launchX = 0;
+//		double launchY = 0;
+//		
+//		//BluetoothConnection bc = new BluetoothConnection(); //must pass this in here somewhere /************used
+//		
+//		//Transmission trans = bc.getTransmission();//new Transmission(); //**********used*******
+//		double goalXBlock = 5;//trans.w1;
+//		double goalYBlock = 9;//trans.w2;
+//		if(goalYBlock > 6){ 
+//			launchXBlock = goalXBlock;
+//			launchYBlock = goalYBlock - 8;
+//			launchingAngle = 0;
+//		}else{
+//			launchXBlock = goalXBlock;
+//			launchYBlock = goalYBlock - 8;
+//			launchingAngle = 180;
+//		}
+//		launchX = launchXBlock * 30;
+//		launchY = launchYBlock * 30;
+//		/* step 1 */
+//		
+//		
+//		/* step 2 DONE */
+//		/* step 3 */
+//		// ???
+//		/* step 4 */// LOCALIZATIONS
+//		//usData.start();
+//		//USLocalizer usLoc = new USLocalizer(odo, control, usData);
+//		//usLoc.start();
+//		LightLocalizer lightLocalizer = new LightLocalizer(odo, navigate, lsl, lsr, sc);
+//		lightLocalizer.doLocalization();
+////		try { Thread.sleep(2000); } catch (InterruptedException e) {}
+//		/* step 5 + step 6, Obstacle avoidance not working yet */
+//			control.travelTo(60,60);
+//			control.turnTo(launchingAngle );
+//		/* step 7 */
+//		BTConnectTest connect = new BTConnectTest();
+//		try {
+//			connect.connect();
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		/* step 8 */
+//		control.travelTo(0, 0);
+//	
 	
 	
 		
