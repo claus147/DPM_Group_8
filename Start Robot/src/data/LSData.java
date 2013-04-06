0eeae5a1936b850c38b4052bdc5f83b597d0dcc1
yuyangliu <liuyuyang0522@gmail.com>
2013-04-06 17:37:11 -0400

    yy upd
    getBalls() added

--- Start Robot/src/data/LSData.java
+++ Start Robot/src/data/LSData.java
@@ -23,7 +23,7 @@ public class LSData implements TimerListener{
 	private int counter = 0; 									//for the array, so it doesnt do null pointer (wrap around)
 	private int threshold;										//threshold level for line detection - tolerance
 	private Timer timer;										//timer for timedOut()
-	private int sleepTime = 50; 								//50 millisecond is optimal time for ls reading sleep
+	private int sleepTime = 10; 								//50 millisecond is optimal time for ls reading sleep
 	private boolean isLine = false;								//boolean for line detection
 	private double percentage;
 	
